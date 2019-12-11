1e8ec78830646132bdea859248292a11cd453177
jude <ouyangzhuhua@126.com>
2019-12-11 17:01:06 +0800

    spring boot basis project initialize

---
+++ springboot-basis/src/main/java/com/ilinux/springbootbasis/SpringbootBasisApplication.java
@@ -0,0 +1,13 @@
+package com.ilinux.springbootbasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBasisApplication.class, args);
+	}
+
+}
---
+++ springboot-basis/src/test/java/com/ilinux/springbootbasis/SpringbootBasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilinux.springbootbasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
